686025f6516e76e4065977a571f7083820ad4860
Black Shark <blacksharkfr@yahoo.fr>
2023-12-09 16:03:55 +0100

    Day09: added inplace solutions

--- 2023/day09/benches/divan_benchmark.rs
+++ 2023/day09/benches/divan_benchmark.rs
@@ -9,22 +9,51 @@ fn main() {
         .main()
 }
 
-mod part1 {
+mod part1_singlethread {
+    use day09::{part1_inplace, part1_recursive_inplace};
+
     use super::*;
     #[divan::bench]
-    fn main(bencher: Bencher) {
+    fn recursive(bencher: Bencher) {
         bencher
             .with_inputs(|| Day::INPUT)
             .bench_values(|input| Day::part1(input))
     }
+
+    #[divan::bench]
+    fn in_place(bencher: Bencher) {
+        bencher
+            .with_inputs(|| Day::INPUT)
+            .bench_values(|input| part1_inplace(input))
+    }
+    #[divan::bench]
+    fn in_place_recursive(bencher: Bencher) {
+        bencher
+            .with_inputs(|| Day::INPUT)
+            .bench_values(|input| part1_recursive_inplace(input))
+    }
 }
 
-mod part2 {
+mod part2_rayon {
+    use day09::{part2_inplace, part2_inplace_recursive};
+
     use super::*;
     #[divan::bench]
-    fn main(bencher: Bencher) {
+    fn recursive(bencher: Bencher) {
         bencher
             .with_inputs(|| Day::INPUT)
             .bench_values(|input| Day::part2(input))
+    }
+    #[divan::bench]
+    fn in_place(bencher: Bencher) {
+        bencher
+            .with_inputs(|| Day::INPUT)
+            .bench_values(|input| part2_inplace(input))
+    }
+    #[divan::bench]
+    fn in_place_recursive(bencher: Bencher) {
+        bencher
+            .with_inputs(|| Day::INPUT)
+            .bench_values(|input| part2_inplace_recursive(input))
     }
 }
--- 2023/day09/src/lib.rs
+++ 2023/day09/src/lib.rs
@@ -1,5 +1,4 @@
 use aoc::Aoc;
-use itertools::Itertools;
 use rayon::prelude::*;
 
 pub struct Day;
@@ -13,11 +12,11 @@ impl Aoc for Day {
 
     fn part1(input: &str) -> Self::OUTPUT {
         input
-            .par_lines()
+            .lines()
             .map(|line| {
                 let (_, values) = parsers::values(line)
                     .unwrap_or_else(|e| panic!("Parser failed {e:?} on line {line}"));
-                predict_next(&values)
+                predict_next::recursive_vec(&values)
             })
             .sum()
     }
@@ -29,22 +28,120 @@ impl Aoc for Day {
                 let (_, mut values) = parsers::values(line)
                     .unwrap_or_else(|e| panic!("Parser failed {e:?} on line {line}"));
                 values.reverse();
-                predict_next(&values)
+                predict_next::recursive_vec(&values)
             })
             .sum()
     }
 }
 
-fn predict_next(values: &[i32]) -> i32 {
-    let differences = values
-        .windows(2)
-        .map(|numbers| numbers[1] - numbers[0])
-        .collect_vec();
-    match differences.iter().all(|number| *number == 0) {
-        true => values.last().cloned(),
-        false => values.last().map(|n| *n + predict_next(&differences)),
+pub fn part1_inplace(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let (_, values) = parsers::values(line)
+                .unwrap_or_else(|e| panic!("Parser failed {e:?} on line {line}"));
+            predict_next::inplace(values)
+        })
+        .sum()
+}
+pub fn part2_inplace(input: &str) -> i32 {
+    input
+        .par_lines()
+        .map(|line| {
+            let (_, mut values) = parsers::values(line)
+                .unwrap_or_else(|e| panic!("Parser failed {e:?} on line {line}"));
+            values.reverse();
+            predict_next::inplace(values)
+        })
+        .sum()
+}
+pub fn part1_recursive_inplace(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let (_, mut values) = parsers::values(line)
+                .unwrap_or_else(|e| panic!("Parser failed {e:?} on line {line}"));
+            predict_next::recursive_inplace(&mut values)
+        })
+        .sum()
+}
+pub fn part2_inplace_recursive(input: &str) -> i32 {
+    input
+        .par_lines()
+        .map(|line| {
+            let (_, mut values) = parsers::values(line)
+                .unwrap_or_else(|e| panic!("Parser failed {e:?} on line {line}"));
+            values.reverse();
+            predict_next::recursive_inplace(&mut values)
+        })
+        .sum()
+}
+
+/// Different algorithms to solve the puzzle
+mod predict_next {
+
+    /// First naive attempt : stores the differences in a new vector and then recurse
+    pub fn recursive_vec(values: &[i32]) -> i32 {
+        let Some(last) = values.last().cloned() else {
+            panic!("Failed to predict next number : diffs do not converge to zero")
+        };
+
+        let differences = values
+            .windows(2)
+            .map(|numbers| numbers[1] - numbers[0])
+            .collect::<Vec<_>>();
+
+        match differences.iter().all(|number| *number == 0) {
+            true => last,
+            false => last + recursive_vec(&differences),
+        }
     }
-    .expect("Failed to predict next number : recursion reached an empty series")
+
+    /// Reuses the same vector
+    ///
+    /// On each loop :
+    /// - the last item is preserved
+    /// - the n-1 items are replaced with their diff
+    ///
+    /// Once the diffs are all 0,
+    /// - walk up the vector to add the successive diffs
+    /// - the final answer is in the last item
+    pub fn inplace(mut values: Vec<i32>) -> i32 {
+        for last in (0..values.len() - 1).rev() {
+            for i in 0..=last {
+                values[i] = values[i + 1] - values[i];
+            }
+
+            if values[0..last].iter().all(|value| *value == 0) {
+                return values[last..].iter().cloned().sum();
+            }
+        }
+        panic!("Failed to predict next number : diffs do not converge to zero")
+    }
+
+    /// Reuses the same vector : recursive version
+    ///
+    /// On each recursion :
+    /// - the last item is preserved
+    /// - the n-1 items are replaced with their diff
+    ///
+    /// Once the diffs are all 0,
+    /// - return the last item up the recursion chain
+    /// - add it to the last item of the previous recursion and return it
+    pub fn recursive_inplace(values: &mut [i32]) -> i32 {
+        if values.iter().all(|n| *n == 0) {
+            if values.is_empty() {
+                panic!("Failed to predict next number : diffs do not converge to zero")
+            }
+            return 0;
+        }
+
+        let end = values.len() - 1;
+        for i in 0..end {
+            values[i] = values[i + 1] - values[i];
+        }
+        values[end] + recursive_inplace(&mut values[0..end])
+    }
 }
 
 mod parsers {
@@ -66,6 +163,42 @@ mod tests {
     #[test]
     fn test_part1() {
         Day::test_part1(114)
+    }
+
+    #[test]
+    fn test_in_place() {
+        let input = Day::SAMPLE_PART1;
+        assert_eq!(114, part1_inplace(input));
+
+        let input = Day::INPUT;
+        assert_eq!(1992273652, part1_inplace(input));
+
+        let input = Day::SAMPLE_PART2;
+        assert_eq!(2, part2_inplace(input));
+
+        let input = Day::INPUT;
+        assert_eq!(1012, part2_inplace(input));
+    }
+
+    #[test]
+    fn test_in_place_recursive() {
+        let input = Day::SAMPLE_PART1;
+        assert_eq!(114, part1_recursive_inplace(input));
+
+        let input = Day::INPUT;
+        assert_eq!(1992273652, part1_recursive_inplace(input));
+
+        let input = Day::SAMPLE_PART2;
+        assert_eq!(2, part2_inplace_recursive(input));
+
+        let input = Day::INPUT;
+        assert_eq!(1012, part2_inplace_recursive(input));
+    }
+
+    #[test]
+    #[should_panic]
+    fn empty_in_place_recursive() {
+        predict_next::recursive_inplace(&mut Vec::new());
     }
 
     #[test]
