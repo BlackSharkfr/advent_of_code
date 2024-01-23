fc4a7bc24a24c119e0b5cc34a345e0eac8546c35
BlackShark <blacksharkfr@yahoo.fr>
2024-01-23 12:50:28 +0100

    Day18 setup

---
+++ 2023/day18/src/lib.rs
@@ -0,0 +1,34 @@
+use aoc::Aoc;
+
+pub struct Day;
+
+impl Aoc for Day {
+    type OUTPUT = u32;
+    const DAY_NUMBER: u8 = 18;
+    const INPUT: &'static str = include_str!("../inputs/input.txt");
+    const SAMPLE_PART1: &'static str = include_str!("../inputs/sample1.txt");
+    const SAMPLE_PART2: &'static str = include_str!("../inputs/sample2.txt");
+
+    fn part1(_input: &str) -> Self::OUTPUT {
+        todo!()
+    }
+
+    fn part2(_input: &str) -> Self::OUTPUT {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        Day::test_part1(0)
+    }
+
+    #[test]
+    fn test_part2() {
+        Day::test_part2(0)
+    }
+}
---
+++ 2023/day18/src/main.rs
@@ -0,0 +1,6 @@
+use aoc::Aoc;
+use day18::Day;
+
+fn main() {
+    Day::main()
+}
